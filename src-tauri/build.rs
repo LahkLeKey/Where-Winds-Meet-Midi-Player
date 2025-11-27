ca6b0a7f5841ea68dc295c32e95bf8519c9b9088
Lumio <youtrueza@gmail.com>
2025-11-27 21:31:30 +0700

    run as admin

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,13 @@
 fn main() {
-    tauri_build::build()
+    // Embed the Windows manifest to require admin privileges
+    #[cfg(windows)]
+    {
+        let mut windows = tauri_build::WindowsAttributes::new();
+        windows = windows.app_manifest(include_str!("app.manifest"));
+        tauri_build::try_build(tauri_build::Attributes::new().windows_attributes(windows))
+            .expect("failed to run build script");
+    }
+
+    #[cfg(not(windows))]
+    tauri_build::build();
 }
